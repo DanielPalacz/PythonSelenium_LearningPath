15a2aa36173a8b1eef9ba1fc1fade4bd4ec27c3b
DanielPalacz <daniel.palacz@gmail.com>
2020-12-19 02:23:24 +0100

    Adding logging functionality to Page object model - part 2 (125__page_object_pattern__example2).

--- 8 - PAGE OBJECT MODEL/125__page_object_pattern__example2/pages/search_hotel.py
+++ 8 - PAGE OBJECT MODEL/125__page_object_pattern__example2/pages/search_hotel.py
@@ -1,7 +1,10 @@
+import logging
+
 
 class SearchHotelPage:
 
     def __init__(self, webdriver):
+        self.logger = logging.getLogger(__name__)
         self.driver = webdriver
         self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
         self.search_hotel_input_xpath = "//div[@id='select2-drop']//input"
@@ -14,15 +17,18 @@ class SearchHotelPage:
         self.search_button_xpath = "//button[text()=' Search']"
 
     def set_city(self, city="Dubai"):
+        self.logger.info(f"Setting city {city}.")
         self.driver.find_element_by_xpath(self.search_hotel_span_xpath).click()
         self.driver.find_element_by_xpath(self.search_hotel_input_xpath).send_keys(city)
         self.driver.find_element_by_xpath(self.location_match_xpath).click()
 
     def set_date_range(self, checkin, checkout):
+        self.logger.info(f"Setting check in/out dates: {checkin}, {checkout}.")
         self.driver.find_element_by_name(self.checkin_input_name).send_keys(checkin)
         self.driver.find_element_by_name(self.checkout_input_name).send_keys(checkout)
 
     def set_travellers(self, adults, child):
+        self.logger.info(f"Setting travellers adults/children quantity: {adults}, {child}.")
         self.driver.find_element_by_id(self.travellers_input_id).click()
         self.driver.find_element_by_id(self.adult_input_id).clear()
         self.driver.find_element_by_id(self.adult_input_id).send_keys(adults)
@@ -30,4 +36,5 @@ class SearchHotelPage:
         self.driver.find_element_by_id(self.children_input_id).send_keys(child)
 
     def perform_search(self):
+        self.logger.info("Performing search.")
         self.driver.find_element_by_xpath(self.search_button_xpath).click()
--- 8 - PAGE OBJECT MODEL/125__page_object_pattern__example2/pages/search_results.py
+++ 8 - PAGE OBJECT MODEL/125__page_object_pattern__example2/pages/search_results.py
@@ -1,16 +1,26 @@
+import logging
 
 
 class SearchResultsPage:
 
     def __init__(self, webdriver):
+        self.logger = logging.getLogger(__name__)
         self.driver = webdriver
         self.hotel_names_xpath = "//h4[contains(@class, 'list_title')]//b"
         self.hotel_prices_xpath = "//div[contains(@class, 'price_tab')]//b"
 
     def get_hotel_names(self):
         hotels = self.driver.find_elements_by_xpath(self.hotel_names_xpath)
-        return [hotel.get_attribute("textContent") for hotel in hotels]
+        hotel_names = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Available hotels:")
+        for hotel_name in hotel_names:
+            self.logger.info(hotel_name)
+        return hotel_names
 
     def get_hotel_prices(self):
         prices = self.driver.find_elements_by_xpath(self.hotel_prices_xpath)
-        return [price.get_attribute("textContent") for price in prices]
+        hotel_prices = [price.get_attribute("textContent") for price in prices]
+        self.logger.info("Hotels prices:")
+        for hotel_price in hotel_prices:
+            self.logger.info(hotel_price)
+        return hotel_prices
